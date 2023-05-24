4f325296ceee45587cb1f4a15d8e7e5b0096e674
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-24 11:28:55 -0300

    feat: implementação menu_data

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,27 @@
-# Req 3
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import csv
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.items = self.r_file(source_path)
+        self.dishes = self.generate_menu()
+
+    def r_file(self, path: str):
+        with open(path, "r") as file:
+            menu = csv.DictReader(file)
+            return list(menu)
+
+    def generate_menu(self):
+        menu = {}
+        for item in self.items:
+            dish = Dish(item["dish"], float(item["price"]))
+            ingredient = Ingredient(item["ingredient"])
+            amount = int(item["recipe_amount"])
+            if dish not in menu:
+                menu[dish] = dish
+            menu[dish].add_ingredient_dependency(
+                ingredient, amount
+            )
+        return set(menu.values())
